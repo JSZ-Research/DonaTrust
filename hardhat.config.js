8bf62e30a24011be67fb807f04c16a4af3168dfe
ANOYUME91 <xulinker@gmail.com>
2026-02-14 03:20:51 -0500

    add hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.24",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+           
+            accounts: {
+                count: 20,
+            },
+        },
+        
+    },
+};
